9c7239c8d36e47b9d335954c157e7a506d75a15a
Luis Eduardo <luiswag.20001@gmail.com>
2022-05-15 12:04:59 -0500

    Funcion que indica como y que elementos conformaran la pagina

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+#Se agrego una lista de elementos llamada tareasIn que se ultilizara por dentro del codigo
+tareasIn = ["luis","jaque","juan"]
 # Create your views here.
+
+#Despues se crean una funcion de tiene como nombre index, esta funcion renderizara el html de index el cual dara la estructura de esa pagina y despues se pasa un dicionario como argumento con un elemento con nombre tareaOut el cual se podra utilizar a dentro de html y se le pasa la lista del inicio tareaIn
+def index(request):
+    return render(request, "tareas/index.html", {
+        "tareasOut": tareasIn
+    })
