74176ba80547d63c2f5ac3b15c92f3c08b2d890c
Luis Eduardo <luiswag.20001@gmail.com>
2022-05-15 11:12:58 -0500

    Update

--- tareas/urls.py
+++ tareas/urls.py
@@ -6,7 +6,7 @@ from django.urls.resolvers import URLPattern
 #Luego para acceder desde la misma carpeta se ustiliza el punto: . , despues se importa los views
 from . import views
 
-#Se crean un dictionario para almacenar los paths 
+#Se crean una lista para almacenar los paths 
 urlpatterns = [
     #En la funcion path recibe como primer argumento un string que recibira al final de la extension que impemento en el urls.py de la carpeta del proyecto 
     #En este caso despues de tareas/ y ira vacio porque es la pagina index osea principal
