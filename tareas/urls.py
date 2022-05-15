de836b47c5ad85931a361dcb5818f8bf93748145
Luis Eduardo <luiswag.20001@gmail.com>
2022-05-15 11:00:11 -0500

    app tareas

---
+++ tareas/urls.py
@@ -0,0 +1,4 @@
+#Se debe agregar esta libreria de django.urls para importar path
+from django.urls import path
+
+#Luego para acceder desde la misma carpeta se ustiliza el punto: . , despues se importa los views
